b76939314e49cec7b22d17845a56f0a111ee4362
aoeu <travis@mcdem.us>
2015-12-05 19:57:18 -0500

    Increase performance by an order of magnitude
    * Remove go routines that randomize disk access.
    
        Despite being concurrent and running against an SSD, the program ran slower.
    
    * Keep channels and a select to bail out as soon as there is an error.
    
        Really, a hard os.Exit from within one of the walk functions would suffice.
    
    * Use functions from filepath package to walk directory tree, but without sorting directories.
    
        Determinism is being traded off for performance, but at a signifcant
        increase in speed when directories of many files are involved.

--- locate.go
+++ locate.go
@@ -3,11 +3,9 @@ package main
 import (
 	"errors"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"path/filepath"
 	"regexp"
-	"sync"
 )
 
 var usage string = `
@@ -15,8 +13,13 @@ Usage: '%s regexp', where "regexp" is a regular expression to match a file name.
 Example: %s 'example.*.txt'
 `
 
-var filenameRegexp *regexp.Regexp
-var paths chan string
+var (
+	filenameRegexp *regexp.Regexp
+	paths          chan string
+	errs           chan error
+	errNotFound    = errors.New("File not found with name matching '%s'\n")
+	done           bool
+)
 
 func main() {
 	if len(os.Args) != 2 {
@@ -30,61 +33,103 @@ func main() {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-	errors := make(chan error)
+	errs = make(chan error)
 	paths = make(chan string)
-	dirs, err := ioutil.ReadDir(wd)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-	}
-	var wg sync.WaitGroup
-	for _, info := range dirs {
-		switch {
-		case info.IsDir():
-			wg.Add(1)
-			go func() {
-				if err := filepath.Walk(wd, Mark); err != nil {
-					errors <- err
-				}
-				wg.Done()
-			}()
-		case filenameRegexp.Match([]byte(info.Name())):
-			paths <- fmt.Sprintf("%v/%v", wd, info.Name())
-		}
-	}
 	go func() {
-		wg.Wait()
-		errors <- errNotFound
+		errs <- Walk(wd, Mark)
 	}()
-	select {
-	case path := <-paths:
-		fmt.Println(filepath.Dir(path))
-		os.Exit(0)
-	case err := <-errors:
-		switch err {
-		case errNotFound:
-			fmt.Fprintf(os.Stderr, err.Error(), os.Args[1])
+	for i := 0; i < 2; i++ {
+		select {
+		case path := <-paths:
+			fmt.Println(filepath.Dir(path))
 			os.Exit(0)
-		default:
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
-		case nil:
+		case err := <-errs:
+			switch err {
+			case errNotFound:
+				fmt.Fprintf(os.Stderr, err.Error(), os.Args[1])
+				os.Exit(0)
+			default:
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			case nil:
+			}
 		}
-
 	}
 }
 
-var (
-	errNotFound = errors.New("File not found with name matching '%s'\n")
-)
-
 func Mark(path string, info os.FileInfo, err error) error {
 	switch {
-	case info.IsDir():
+	case done || info.IsDir():
 		return nil
 	case err != nil && err != os.ErrPermission:
 		return err
 	case filenameRegexp.Match([]byte(info.Name())):
+		done = true
 		paths <- path
+		return nil
+	}
+	return errNotFound
+}
+
+// Code below this line is an excerpt from the standard library package named "filepath."
+
+func Walk(root string, walkFn filepath.WalkFunc) error {
+	info, err := os.Lstat(root)
+	if err != nil {
+		return walkFn(root, nil, err)
+	}
+	return walk(root, info, walkFn)
+}
+
+var SkipDir = errors.New("skip this directory")
+
+func walk(path string, info os.FileInfo, walkFn filepath.WalkFunc) error {
+	err := walkFn(path, info, nil)
+	if err != nil && err != errNotFound {
+		if info.IsDir() && err == SkipDir {
+			return nil
+		}
+		return err
+	}
+
+	if !info.IsDir() {
+		return nil
+	}
+
+	names, err := readDirNames(path)
+	if err != nil {
+		return walkFn(path, info, err)
+	}
+
+	for _, name := range names {
+		filename := filepath.Join(path, name)
+		fileInfo, err := os.Lstat(filename)
+		if err != nil {
+			if err := walkFn(filename, fileInfo, err); err != nil && err != SkipDir {
+				return err
+			}
+		} else {
+			err = walk(filename, fileInfo, walkFn)
+			if err != nil {
+				if !fileInfo.IsDir() || err != SkipDir {
+					return err
+				}
+			}
+		}
 	}
 	return nil
+}
+
+func readDirNames(dirname string) ([]string, error) {
+	f, err := os.Open(dirname)
+	if err != nil {
+		return nil, err
+	}
+	names, err := f.Readdirnames(-1)
+	f.Close()
+	// Unlike filepath's readDirNames function, do not sort directory names here.
+	if err != nil {
+		return nil, err
+	}
+	return names, nil
 }
