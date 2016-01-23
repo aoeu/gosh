97f7620c9ee5636ead6b7184f3759947be8434f2
aoeu <travis@mcdem.us>
2016-01-23 16:28:11 -0500

    Add tool to print paths from shorthand

---
+++ path.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func exists(path string) bool {
+	if _, err := os.Stat(path); err != nil {
+		return false
+	}
+	return true
+}
+
+func prependIfExists(prefix string, path *string) bool {
+	if _, err := os.Stat(prefix + *path); err != nil {
+		return false
+	}
+	*path = prefix + *path
+	return true
+}
+
+func main() {
+	dir := strings.Join(os.Args[1:], "/")
+	switch {
+	case exists(dir) ||
+		prependIfExists(os.Getenv("HOME")+"/", &dir) ||
+		prependIfExists("/", &dir):
+		fmt.Println(dir)
+	default:
+		fmt.Fprintf(os.Stderr, "Unknown path: %v\n", dir)
+		os.Exit(1)
+	}
+}
