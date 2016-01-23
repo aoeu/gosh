5ee028e318b562874146afbeee38c319fd8e4c83
aoeu <travis@mcdem.us>
2016-01-23 18:49:24 -0500

    Update usage and add pro-tip

--- cmd/path/path.go
+++ cmd/path/path.go
@@ -3,10 +3,32 @@ package main
 // go get github.com/aoeu/gosh/cmd/path && echo "function goto { cd $(path $*); }" >> ~/.profile && source ~/.profile
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"strings"
 )
+
+var usageMessage = `usage: %v path to some dir
+
+%v takes a space separated list of directory names of a valid directory tree and navigates and prints the full path with separators specific to the host Operating System.
+
+If the directory names do not create a complete path, a path under the user's home directory is attempted, then a path derived from the root directory, and finally an error is printed if none are found to be valid paths.
+
+example: path go src encoding json 
+
+go get github.com/aoeu/gosh/cmd/path 
+echo "function goto { cd $(path $*); }" >> ~/.profile 
+source ~/.profile
+goto go src net
+
+`
+
+func usage() {
+	s := os.Args[0]
+	fmt.Fprintf(os.Stderr, usageMessage, s, s)
+	os.Exit(2)
+}
 
 func exists(path string) bool {
 	if _, err := os.Stat(path); err != nil {
@@ -24,6 +46,8 @@ func prependIfExists(prefix string, path *string) bool {
 }
 
 func main() {
+	flag.Usage = usage
+	flag.Parse()
 	dir := strings.Join(os.Args[1:], "/")
 	switch {
 	case exists(dir) ||
