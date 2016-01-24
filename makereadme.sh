911dc589d3c2421b70ec7d00103f0a1650b85142
aoeu <travis@mcdem.us>
2016-01-23 19:09:23 -0500

    Add README file generator

---
+++ makereadme.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+out=README.md
+
+cat > $out << EOF
+
+# gosh
+
+Go programs to augment shell usage. Oh, my.
+
+## Installation and Usage
+
+* Install Go: \`https://golang.org/doc/install\`
+* Open a terminal window and run a shell.
+* Install an individual program: \`go get github.com/aoeu/gosh/cmd/trash\`
+* Run the help page of the program: \`trash -help\`
+
+Alternatively, install all the programs:  
+\`go get github.com/aoeu/gosh/cmd/...\`
+
+
+## Program Descriptions
+
+EOF
+for f in `lc cmd/`; do echo "### $f  " && echo "\`\`\`" && $f -help 2>&1 || echo "\`\`\`" ; done >> $out
+
