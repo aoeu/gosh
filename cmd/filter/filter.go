b7c61536121f35a468020b6cff9d7b1dffa4c093
Travis McDemus <travis@mcdem.us>
2016-05-15 18:55:18 -0400

    Add flag to filter only when matching all
    Also fix bugs in documentation usage template
    and add another example.
    
    h/t @ifo for pairing.

--- cmd/filter/filter.go
+++ cmd/filter/filter.go
@@ -17,7 +17,10 @@ text do not contain the provided text tokens are printed to
 standard output.
 
 examples:
-	find . -name '*.yava' | filter generated-sources target test
+	find . -name '*.yava' | {{.}} generated-sources target test
+	cat works_of_shakespeare.txt | {{.}} thou thee thine
+
+flags:
 
 `
 
@@ -32,28 +35,39 @@ func usage() {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-	if err := t.Execute(os.Stdout, os.Args[0]); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
+	// TODO(aoeu): Print default flags before printing usage examples.
 	flag.PrintDefaults()
 	os.Exit(2)
 }
 
 func main() {
 	flag.Usage = usage
+	args := struct {
+		matchAll bool
+	}{}
+	flag.BoolVar(&args.matchAll, "all", false, "Lines ommitted must match all filters (instead of any filter).")
 	flag.Parse()
-	filters := os.Args
+	filters := flag.Args()
+	if args.matchAll && len(filters) == 0 {
+		flag.Usage()
+	}
+
 	input := bufio.NewScanner(os.Stdin)
 	for input.Scan() {
 		t := input.Text()
-		isMatch := false
+		matchedAny := false
+		matchedAll := true
 		for _, f := range filters {
 			if strings.Contains(t, f) {
-				isMatch = true
+				matchedAny = true
+			} else {
+				matchedAll = false
 			}
 		}
-		if !isMatch {
+		switch {
+		case args.matchAll && matchedAll:
+		case !args.matchAll && matchedAny:
+		default:
 			fmt.Fprintln(os.Stdout, t)
 		}
 	}
